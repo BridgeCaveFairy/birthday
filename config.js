f03f9c092746eeee03cc62fab793362310bc21cb
Chen Zhihui <143569962+BridgeCaveFairy@users.noreply.github.com>
2024-05-28 23:46:34 +0800

    Update config.js

--- config.js
+++ config.js
@@ -42,7 +42,7 @@ var config = {
     },
     // 按钮文字描述, 以下是默认的按钮文字，英文的，您可以改成你喜欢的文字
     desc: {
-        turn_on: "🎂点这里开始🎂",
+        turn_on: "🎂点这里开始哦🎂",
         play: "🎵来点音乐🎵",
         bannar_coming: "👑带上皇冠👑",
         balloons_flying: "🎈BALLON🎈",
